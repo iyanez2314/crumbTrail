4e7986a2d143aee107a972cdca6728e2f3d47270
Isaac Yanez <isaac231467@gmail.com>
2023-10-28 10:46:30 -0500

    added some code to handle the go back from other screens to the home page

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,16 +1,25 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Pressable } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { LoginScreenNavigationProp } from "../screens/LoginScreen";
 
 interface Props {
-  navigation: any;
+  navigation?: any;
   title: string;
 }
 
-export default function Heading({ title }: Props) {
+export default function Heading({ navigation, title }: Props) {
+  const goBack = () => {
+    navigation.navigate("Home");
+  };
   return (
     <TouchableOpacity>
       <View className="flex flex-row items-center gap-2 ml-1">
+        <Pressable
+          onPress={goBack}
+          className="w-[44px] h-[44px] items-center justify-center"
+        >
+          <Ionicons name="ios-caret-back-outline" size={24} color="black" />
+        </Pressable>
         <Text className="text-2xl font-bold">{title}</Text>
       </View>
     </TouchableOpacity>
